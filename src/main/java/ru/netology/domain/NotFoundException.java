3e0b57b25589754dbd190fc18df20e764db44b5e
ElenaMughi <elena.mughi@gmail.com>
2021-12-31 15:06:31 +0300

    Start

---
+++ src/main/java/ru/netology/domain/NotFoundException.java
@@ -0,0 +1,10 @@
+package ru.netology.domain;
+
+public class NotFoundException extends RuntimeException  {
+
+    public NotFoundException(String s) {
+        super(s);
+//        super.getMessage();
+//        System.out.println(s);
+    }
+}
---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,43 @@
+package ru.netology.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+
+class ProductRepositoryTest {
+
+    private ProductRepository repository = new ProductRepository();
+    private Book coreJava = new Book(2, "Core Java", 200, "Author1", 200, 2021);
+    private Book python = new Book(1, "Python", 100, "Author", 100, 2021);
+
+    @Test
+    public void shouldSaveOneItem() {
+        repository.save(coreJava);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveOneItem() {
+        repository.save(coreJava);
+        repository.save(python);
+        repository.removeById(1);
+        Product[] expected = new Product[]{coreJava};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotRemoveOneItem() {
+        repository.save(coreJava);
+        repository.save(python);
+        repository.removeById(2);
+        Product[] expected = new Product[]{python};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+}
